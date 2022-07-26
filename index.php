85e73b3cb8f6fe1f6f20ad8e4b56621d8ba35643
Nikki Cece Ilanan <75845052+ncece11@users.noreply.github.com>
2022-07-26 15:07:01 +0800

    remove index html

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-	include 'header.php' 
+	require_once 'header.php'; 
 ?>
 
 <h1>HELLO WORLD!</h1>
@@ -9,5 +9,5 @@
 <p class="para">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 
 <?php 
-	include 'footer.php'
+	require_once 'footer.php';
 ?>
